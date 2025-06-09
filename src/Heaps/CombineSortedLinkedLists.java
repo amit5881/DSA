0e4c1f3b0f4ea5b2ad6ba65324ca94ddb5526592
amit5881 <amitmallick2010@gmail.com>
2025-06-09 19:34:37 +0530

    heap

---
+++ src/Heaps/CombineSortedLinkedLists.java
@@ -0,0 +1,50 @@
+package Heaps;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class CombineSortedLinkedLists {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static Node combineSortedLinkedLists(List<Node> heads) {
+        PriorityQueue<Node> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a.data));
+        for (Node head : heads) if (head != null) minHeap.add(head);
+
+        Node dummy = new Node(-1);
+        Node current = dummy;
+        while (!minHeap.isEmpty()) {
+            Node smallestNode = minHeap.poll();
+            current.next = smallestNode;
+            current = current.next;
+
+            if (smallestNode.next != null) minHeap.add(smallestNode.next);
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Node head1 = new Node(1, new Node(6));
+        Node head2 = new Node(1, new Node(4, new Node(6)));
+        Node head3 = new Node(3, new Node(7));
+
+        Node result = combineSortedLinkedLists(List.of(head1, head2, head3));
+        while (result != null) {
+            System.out.print(result.data + " -> ");
+            result = result.next;
+        }
+    }
+}
---
+++ src/Heaps/MedianOfAnIntegerStream.java
@@ -0,0 +1,34 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class MedianOfAnIntegerStream {
+
+    PriorityQueue<Integer> leftHalf = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+    PriorityQueue<Integer> rightHalf = new PriorityQueue<>();
+
+    public void add(int num) {
+        if (leftHalf.isEmpty() || num <= leftHalf.peek()) {
+            leftHalf.offer(num);
+            if (leftHalf.size() > rightHalf.size() + 1) rightHalf.add(leftHalf.poll());
+        } else {
+            rightHalf.offer(num);
+            if (rightHalf.size() > leftHalf.size()) leftHalf.add(rightHalf.poll());
+        }
+    }
+
+    public float getMedian() {
+       if (leftHalf.size() == rightHalf.size()) return (float) (leftHalf.peek() + rightHalf.peek()) / 2;
+       return leftHalf.peek();
+    }
+
+    public static void main(String[] args) {
+        MedianOfAnIntegerStream medianOfAnIntegerStream = new MedianOfAnIntegerStream();
+        medianOfAnIntegerStream.add(3);
+        medianOfAnIntegerStream.add(6);
+        System.out.println(medianOfAnIntegerStream.getMedian());
+        medianOfAnIntegerStream.add(1);
+        System.out.println(medianOfAnIntegerStream.getMedian());
+    }
+
+}
