c4f888e40f17408b9061856e3f6aecfa83266674
amit5881 <amitmallick2010@gmail.com>
2025-06-16 01:27:02 +0530

    retry

--- src/ByteByteGo/Intervals/LargestOverlapOfIntervals.java
+++ src/ByteByteGo/Intervals/LargestOverlapOfIntervals.java
@@ -16,26 +16,24 @@ public class LargestOverlapOfIntervals {
         }
     }
 
+    // sweeping line algorithm
     public static int largestOverlapOfIntervals(List<List<Integer>> intervals) {
         List<Point> points = new ArrayList<>();
         for (List<Integer> interval : intervals) {
             points.add(new Point(interval.getFirst(), 's'));
             points.add(new Point(interval.getLast(), 'e'));
         }
-
         points.sort((p1, p2) -> p1.time != p2.time ? Integer.compare(p1.time, p2.time) : Character.compare(p1.type, p2.type));
 
-        int activeIntervals = 0;
-        int maxOverlaps = 0;
+        int activeIntervals = 0, maxOverlaps = 0;
         for (Point point : points) {
             if (point.type == 's') {
                 activeIntervals++;
             } else {
                 activeIntervals--;
             }
-            maxOverlaps = Math.max(maxOverlaps, activeIntervals);
+            maxOverlaps = Math.max(activeIntervals, maxOverlaps);
         }
-
         return maxOverlaps;
     }
 
