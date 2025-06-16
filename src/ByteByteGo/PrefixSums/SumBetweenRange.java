100f6d2096d037423eedf4281ad101d5467a42f5
amit5881 <amitmallick2010@gmail.com>
2025-06-16 23:52:35 +0530

    Prefix sum1

---
+++ src/ByteByteGo/PrefixSums/SumBetweenRange.java
@@ -0,0 +1,19 @@
+package ByteByteGo.PrefixSums;
+
+public class SumBetweenRange {
+
+    public static int sumBetweenRange(int[] nums, int i, int j) {
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+
+        for (int k = 1; k < nums.length; k++) prefixSum[k] = nums[k] + prefixSum[k - 1];
+
+        return (i == 0) ? prefixSum[j] : prefixSum[j] - prefixSum[i - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumBetweenRange(new int[]{3, -7, 6, 0, -2, 5}, 0, 3));
+        System.out.println(sumBetweenRange(new int[]{3, -7, 6, 0, -2, 5}, 2, 4));
+        System.out.println(sumBetweenRange(new int[]{3, -7, 6, 0, -2, 5}, 2, 2));
+    }
+}
