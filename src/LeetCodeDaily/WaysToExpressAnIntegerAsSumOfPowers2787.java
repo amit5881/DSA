dc5dc2339c5f126575af8f92eabae190ca6ecbd2
amit5881 <amitmallick2010@gmail.com>
2025-09-23 11:38:35 +0530

    remove unused import

--- src/LeetCodeDaily/WaysToExpressAnIntegerAsSumOfPowers2787.java
+++ src/LeetCodeDaily/WaysToExpressAnIntegerAsSumOfPowers2787.java
@@ -3,7 +3,6 @@ package LeetCodeDaily;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class WaysToExpressAnIntegerAsSumOfPowers2787 {
 
